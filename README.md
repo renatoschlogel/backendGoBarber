6799f29706e81ba04c1fdef898d9d7d94046e512
Renato W Schlogel <renato.s@outlook.com>
2020-05-02 01:09:28 -0300

    Cadastro de Agendamentos

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Go Barber API
+# Go Barber API
+
+
+## Roteiro da Construção (Ao final sera transferido para o notion):
+
+  1 - A dicionar a lib uuidv4 para gerar o id : yarn add uuidv4;
 
 
 <hr>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,6 @@ import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({
-    mensagem: 'Parabéns :D, aplicação respondendo com sucesso!',
-  });
-});
-
 routes.use('/appointments', appointmentsRouter);
 
 export default routes;
