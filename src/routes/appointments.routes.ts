b1c014bf1d50124494a7a14fe9587ea71b5eed96
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-05-03 23:57:01 -0300

    Trabalhando com Dados - Conceito de DTO

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -24,7 +24,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This Appointment is already booked' });
   }
 
-  const appointment = appointmentRepository.create(provider, parsedDate);
+  const appointment = appointmentRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
