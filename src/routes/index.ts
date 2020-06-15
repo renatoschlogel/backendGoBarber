6985f29d124c5eca71005162947a3c4c24ea27ac
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-06-15 00:06:37 -0300

    Upload de Imagens - Servindo arquivos estaticos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 
 import sessionsRouter from './sessions.routes';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import uploadConfig from '../config/upload';
 
 const routes = Router();
+
+routes.use('/files', express.static(uploadConfig.directory));
 
 routes.use('/sessions', sessionsRouter);
 
